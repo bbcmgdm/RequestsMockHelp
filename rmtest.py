d5e346f88925041bcbd73e61cabeaea2eb4923b2
Michael Maclean <michael.maclean@bbc.co.uk>
2017-06-28 11:42:30 +0100

    Prove adding query string in URL doesn't work either

--- rmtest.py
+++ rmtest.py
@@ -43,3 +43,18 @@ class TestAPIClient(object):
             'foo': 'bar',
             'bar': 'baz'
         }
+
+    def test_qs_in_url(self):
+        adapter, session = get_session()
+        adapter.register_uri('GET', 'mock://test.api.com/things?foo=bar&bar=baz', text='{"foo": "bar"}')
+
+        api = APIClient('mock://test.api.com', session=session)
+        response = api.get('things', foo='bar', bar='baz')
+
+        assert response == {'foo': 'bar'}
+        assert len(adapter.request_history) == 1
+        assert adapter.request_history[0].url == 'mock://test.api.com/things'
+        assert adapter.request_history[0].qs == {
+            'foo': 'bar',
+            'bar': 'baz'
+        }
